524d3f0fa267faf12c1bf6cfdc8be7096dd6885e
J W Murray <james@quaintjames.com>
2021-09-20 16:55:20 +0100

    updates

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,6 +1,7 @@
 const Path = require('path');
 const Webpack = require('webpack');
 const { merge } = require('webpack-merge');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -20,6 +21,11 @@ module.exports = merge(common, {
     new Webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
     }),
+    new ESLintPlugin({
+      extensions: 'js',
+      emitWarning: true,
+      files: Path.resolve(__dirname, '../src'),
+    }),
     new StylelintPlugin({
       files: Path.join('src', '**/*.s?(a|c)ss'),
       config: {
@@ -31,7 +37,9 @@ module.exports = merge(common, {
       },
       fix: true,
     }),
-    new MiniCssExtractPlugin({ filename: 'css/app.css', })
+    new ESLintPlugin({
+      emitWarning: true,
+    }),
   ],
   module: {
     rules: [
