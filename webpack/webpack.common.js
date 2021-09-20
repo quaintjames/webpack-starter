9956f615ad88dc740c19f50fd1f7fc68ad133d74
J W Murray <james@quaintjames.com>
2021-09-20 16:35:19 +0100

    bundle tracker removed

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,7 +2,6 @@ const Path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
   entry: {
